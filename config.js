f91ca3b1c4d0122d80e960caf2f7955bdb24ef05
ShadowQueen06 <malk.lol140@gmail.com>
2026-07-08 02:22:20 +0200

    config.js

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  panelChannelId: "1524204100533616761",
+  ticketCategoryId: "1523399598817673347",
+
+  serverName: "العراق العظيم",
+
+  ticketTypes: {
+    complaint: {
+      label: "شكوى",
+      emoji: "📩",
+      channelPrefix: "شكوى"
+    },
+    purchase: {
+      label: "شراء",
+      emoji: "🛒",
+      channelPrefix: "شراء"
+    },
+    support: {
+      label: "دعم فني",
+      emoji: "🛠️",
+      channelPrefix: "دعم"
+    },
+    question: {
+      label: "استفسار",
+      emoji: "❓",
+      channelPrefix: "استفسار"
+    },
+    partnership: {
+      label: "شراكة",
+      emoji: "🤝",
+      channelPrefix: "شراكة"
+    }
+  }
+};
